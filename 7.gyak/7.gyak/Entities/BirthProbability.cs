af32daba8e09d79af434b9d09ff13e48fbf34cf0
tferencj <torok.ferenc.j@gmail.com>
2020-10-25 10:28:26 +0100

    2

---
+++ 7.gyak/7.gyak/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.gyak.Entities
+{
+    class BirthProbability
+    {
+        public int kor { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+}
---
+++ 7.gyak/7.gyak/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.gyak.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int kor { get; set; }
+        public double Deathprobability { get; set; }
+    }
+}
