8abb02fa0b9252031a5f86cebff993b8b30b6fa9
tferencj <torok.ferenc.j@gmail.com>
2020-11-11 17:26:53 +0100

    7

--- 8.gyak/8.gyak/Form1.cs
+++ 8.gyak/8.gyak/Form1.cs
@@ -13,7 +13,7 @@ namespace _8.gyak
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
+        private List<Ball> _toys = new List<Ball>();
 
         private BallFactory _factory;
         public BallFactory Factory
@@ -29,16 +29,16 @@ namespace _8.gyak
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var toy= Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
                 ball.MoveToy();
                 if (ball.Left > maxPosition)
@@ -47,9 +47,9 @@ namespace _8.gyak
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
         }
     }
